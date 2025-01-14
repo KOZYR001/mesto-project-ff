88b0f5c6601cd2c5be2a4ab370632a17c53dcf55
KOZYR001 <Asmorch11122007@yandex.ru>
2025-01-14 06:30:12 +0400

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+// @todo: Template
+const templateCard = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// @todo: DOM узел
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// @todo: Create Card
+function cardCreate (card, deleteButton) {
+  const cardElement = templateCard.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.alt;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  const buttonRemove = cardElement.querySelector('.card__delete-button');
+  buttonRemove.addEventListener('click',() => deleteCard(cardElement));
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+// @todo: Delete card
+ function deleteCard(cardElement) {
+     cardElement.remove();
+ }
 
-// @todo: Вывести карточки на страницу
+// @todo: вывод карточки на страницу
+initialCards.forEach(function(element) {
+placesList.append(cardCreate(element));
+})
