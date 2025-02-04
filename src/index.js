f351109c1bde524cbb52c2b2b91e16f7713af463
KOZYR001 <Asmorch11122007@yandex.ru>
2025-02-04 23:15:14 +0400

    настройка вебпак

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-const jordanImage = new URL('./images/jordan.jpg', import.meta.url);
-const jamesImage = new URL('./images/james.jpg', import.meta.url);
-const bryantImage = new URL('./images/bryant.jpg', import.meta.url)
-
-const whoIsTheGoat = [
-    { name: 'Michael Jordan', link: './images/jordan.jpg' },
-    { name: 'Lebron James', link: './images/james.jpg' },
-    { name: 'Kobe Bryant', link: './images/bryant.jpg' },
-  ];
-
-
-
 const numbers = [2, 3, 5];
 
 
 const doubledNumbers = numbers.map(number => number * 2);
 
+// @todo: Template
+const templateCard = document.querySelector('#card-template').content;
+
+// @todo: DOM узел
+const placesList = document.querySelector('.places__list');
+
+// @todo: Create Card
+function cardCreate (card, deleteButton) {
+  const cardElement = templateCard.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.alt;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  const buttonRemove = cardElement.querySelector('.card__delete-button');
+  buttonRemove.addEventListener('click',() => deleteCard(cardElement));
+  return cardElement;
+}
+
+// @todo: Delete card
+ function deleteCard(cardElement) {
+     cardElement.remove();
+ }
+
+// @todo: вывод карточки на страницу
+initialCards.forEach(function(element) {
+placesList.append(cardCreate(element));
+})
+
 console.log(doubledNumbers); 
+
+import './pages/index.css';
