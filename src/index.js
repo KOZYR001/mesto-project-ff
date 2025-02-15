920e18f41d7461d4cc636d7fceaa3d49c3862f6e
KOZYR001 <Asmorch11122007@yandex.ru>
2025-02-15 17:23:22 +0400

    sohr8

--- src/index.js
+++ src/index.js
@@ -5,40 +5,48 @@ import { initialCards } from './scripts/cards.js';
 import './scripts/cards.js';
 import {openModal, closeModal, addListener} from './components/modal.js';
 
-
-const popupEditProfile = document.querySelector("popup_type_edit");
-const popupAddCard = document.querySelector("popup_type_new-card");
-const popupImage = document.querySelector("popup_type_image");
+const addCardBtn = document.querySelector(".profile__add-button")
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popupEditProfile = document.querySelector(".popup_type_edit");
+const popupAddCard = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup__image");
 const numbers = [2, 3, 5];
 const doubledNumbers = numbers.map(number => number * 2);
 const templateCard = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
+
 
 addListener(popupEditProfile);
 addListener(popupAddCard);
 addListener(popupImage);
 
 // @todo: Create Card
-function cardCreate (card, deleteButton) {
+function cardCreate (card, handleDelete) {
   const cardElement = templateCard.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__image').alt = card.alt;
   cardElement.querySelector('.card__title').textContent = card.name;
   const buttonRemove = cardElement.querySelector('.card__delete-button');
-  buttonRemove.addEventListener('click',() => deleteButton(cardElement));
+  buttonRemove.addEventListener('click',() => handleDelete(cardElement));
   return cardElement;
 }
 
 // @todo: Delete card
- function deleteCard(cardElement) {
+ function handleDeleteItem(cardElement) {
      cardElement.remove();
  }
 
 // @todo: вывод карточки на страницу
-initialCards.forEach(function(deleteCard) {
-placesList.append(cardCreate(deleteCard));
+initialCards.forEach(function(card) {
+placesList.append(cardCreate(card,handleDeleteItem));
 })
 
-console.log(doubledNumbers); 
+console.log(doubledNumbers);
 
+profileEditButton.addEventListener("click", () => openModal(popupEditProfile));
+addCardBtn.addEventListener("click", () => openModal(popupAddCard));
+
+popupImage.addEventListener("click", () => {
+  openModal(popupImage);
+});
 
